18fa8c53e068476e5d53f5b75c02ab887ad53e5b
Angel Fernandez <afernandez@logtrust.com>
2017-03-22 02:16:05 +0100

    Refactor

--- bin/index.js
+++ bin/index.js
@@ -3,5 +3,6 @@
 const cmdUtils = require('../lib/utils/commandLine.js');
 const app = require('../lib/core/app.js');
 
+//Get command line options
 const options = cmdUtils.getCmdLineOptions();
 app.run(options);
